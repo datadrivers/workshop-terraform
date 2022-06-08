e1a11c523e4cec53f86a84e3eb6aae4c78b16b6b
Andre Moeller <andre.moeller@datadrivers.de>
2022-06-08 10:40:35 +0200

    feat: Site improvements

--- exercises/first-apply/v2/main.tf
+++ exercises/first-apply/v2/main.tf
@@ -2,7 +2,7 @@ locals {
   filename = format("%s/foo.bar", path.module)
 }
 
-resource "local_file" "foo" {
+resource "local_file" "this" {
   content         = var.file_content
   filename        = local.filename
   file_permission = "0644"
